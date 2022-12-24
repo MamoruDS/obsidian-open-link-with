f2ad11dcbebe8a404bf9aafdd24771ea4a2f870f
MamoruDS <mamoruds.io@gmail.com>
2022-12-25 08:53:35 +0900

    modified: browser now handled by `ProfileMgr`

--- src/open.ts
+++ src/open.ts
@@ -1,33 +1,10 @@
 import { spawn } from 'child_process'
-import { platform } from 'os'
 
-import {
-    Browser as _Browser,
-    BrowserProfile,
-} from './types'
-import { PRESET_BROWSERS } from './constant'
 import { log } from './utils'
 
 class OpenErr extends Error {
     constructor(msg: string) {
         super(msg)
-    }
-}
-
-class Browser implements _Browser {
-    name: string
-    profiles: Partial<
-        Record<NodeJS.Platform, BrowserProfile>
-    >
-    customCMD: string
-    constructor(
-        name: string,
-        defaultCMD?: Partial<
-            Record<NodeJS.Platform, BrowserProfile>
-        >
-    ) {
-        this.name = name
-        this.profiles = defaultCMD
     }
 }
 
@@ -87,70 +64,4 @@ const openWith = async (
     return await _spawn(_cmd)
 }
 
-const getPresetBrowser = (): Browser[] => {
-    const presets: Browser[] = []
-    presets.push(
-        new Browser('safari', PRESET_BROWSERS['safari'])
-    )
-    presets.push(
-        new Browser('firefox', PRESET_BROWSERS['firefox'])
-    )
-    presets.push(
-        new Browser('chrome', PRESET_BROWSERS['chrome'])
-    )
-    presets.push(
-        new Browser('chromium', PRESET_BROWSERS['chromium'])
-    )
-    presets.push(
-        new Browser('edge', PRESET_BROWSERS['edge'])
-    )
-    return presets
-}
-
-export const getValidBrowser = async (): Promise<
-    Record<string, string[]>
-> => {
-    const browser = getPresetBrowser()
-    const os = platform()
-    const preset = {} as Record<string, string[]>
-    browser.forEach(async ({ profiles, name }) => {
-        let app = profiles[os]
-        if (
-            typeof app !== 'undefined' &&
-            app.test &&
-            (await app.test(app))
-        ) {
-            for (const pvt of [0, 1]) {
-                const cmds = []
-                if (pvt) {
-                    if (!app?.optional?.private) {
-                        continue
-                    }
-                    app = {
-                        ...app,
-                        ...(app.optional.private ?? {}),
-                    }
-                }
-                if (app.sysCmd) {
-                    cmds.push(app.sysCmd)
-                }
-                if (app.sysArgs) {
-                    app.sysArgs.forEach((arg) =>
-                        cmds.push(arg)
-                    )
-                }
-                cmds.push(app.cmd)
-                if (app.args) {
-                    app.args.forEach((arg) =>
-                        cmds.push(arg)
-                    )
-                }
-                preset[name + (pvt ? '-private' : '')] =
-                    cmds
-            }
-        }
-    })
-    return preset
-}
-
-export { Browser, BrowserProfile, openWith, OpenErr }
+export { openWith, OpenErr }
---
+++ src/profile.ts
@@ -0,0 +1,133 @@
+import { platform } from 'os'
+
+import { PRESET_BROWSERS } from './constant'
+import {
+    Browser as _Browser,
+    BrowserProfile,
+    BrowserProfileBase,
+} from './types'
+
+class Browser implements _Browser {
+    name: string
+    profiles: Partial<
+        Record<NodeJS.Platform, BrowserProfile>
+    >
+    customCMD: string
+    constructor(
+        name: string,
+        defaultCMD?: Partial<
+            Record<NodeJS.Platform, BrowserProfile>
+        >
+    ) {
+        this.name = name
+        this.profiles = defaultCMD
+    }
+    getExecCommands = (
+        platform: NodeJS.Platform
+    ): {
+        main: string[]
+        private?: string[]
+    } => {
+        const res = {} as {
+            main: string[]
+            private?: string[]
+        }
+        let bp: BrowserProfile = this.profiles[platform]
+        for (const pvt of [0, 1]) {
+            const cmds = []
+            let bpBase: BrowserProfileBase
+            if (pvt) {
+                if (!bp?.optional?.private) {
+                    continue
+                }
+                bpBase = {
+                    ...bp,
+                    ...(bp.optional.private ?? {}),
+                }
+            } else {
+                bpBase = bp
+            }
+            if (bpBase.sysCmd) {
+                cmds.push(bpBase.sysCmd)
+            }
+            if (bpBase.sysArgs) {
+                bpBase.sysArgs.forEach((arg) =>
+                    cmds.push(arg)
+                )
+            }
+            cmds.push(bpBase.cmd)
+            if (bpBase.args) {
+                bpBase.args.forEach((arg) => cmds.push(arg))
+            }
+            if (pvt) {
+                res.private = cmds
+            } else {
+                res.main = cmds
+            }
+        }
+        return res
+    }
+}
+
+const getPresetBrowsers = (): Browser[] => {
+    const presets: Browser[] = []
+    presets.push(
+        new Browser('safari', PRESET_BROWSERS['safari'])
+    )
+    presets.push(
+        new Browser('firefox', PRESET_BROWSERS['firefox'])
+    )
+    presets.push(
+        new Browser('chrome', PRESET_BROWSERS['chrome'])
+    )
+    presets.push(
+        new Browser('chromium', PRESET_BROWSERS['chromium'])
+    )
+    presets.push(
+        new Browser('edge', PRESET_BROWSERS['edge'])
+    )
+    return presets
+}
+
+class ProfileMgr {
+    private _preset_browser: Browser[]
+    private _browsers: Browser[]
+    constructor() {
+        this._browsers = []
+    }
+    loadValidPresetBrowsers = async (): Promise<void> => {
+        this._preset_browser = []
+        const presets = getPresetBrowsers()
+        const os = platform()
+        presets.forEach(async (browser) => {
+            const { profiles, name } = browser
+            let app = profiles[os]
+            if (
+                typeof app !== 'undefined' &&
+                app.test &&
+                (await app.test(app))
+            ) {
+                this._preset_browser.push(browser)
+            }
+        })
+    }
+    getBrowsers = (): Browser[] => {
+        return [...this._preset_browser, ...this._browsers]
+    }
+    getBrowsersCMD = (
+        custom: Record<string, string[]>
+    ): Record<string, string[]> => {
+        const res: Record<string, string[]> = {}
+        this.getBrowsers().forEach((browser) => {
+            const cmds = browser.getExecCommands(platform())
+            res[browser.name] = cmds.main
+            if (typeof cmds.private !== 'undefined') {
+                res[browser.name + '-private'] =
+                    cmds.private
+            }
+        })
+        return { ...res, ...custom }
+    }
+}
+
+export { ProfileMgr }
